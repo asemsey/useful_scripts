da31e8bc047a866a53decaecd60b7b2314cbbbeb
Anna Semsey <asemsey@1-D-2.42heilbronn.de>
2024-07-21 11:38:58 +0200

    small correction in classbase

--- class_base.sh
+++ class_base.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MAKEFILE_SOURCE=~/scripts/files/Makefile
-CPP_SOURCE=~/scripts/files/AClass.cpp
-HPP_SOURCE=~/scripts/files/AClass.hpp
+MAKEFILE_SOURCE=~/useful_scripts/files/Makefile
+CPP_SOURCE=~/useful_scripts/files/AClass.cpp
+HPP_SOURCE=~/useful_scripts/files/AClass.hpp
 
 DESTINATION_DIR=$PWD
 
@@ -16,8 +16,8 @@ cp $MAKEFILE_SOURCE $DESTINATION_DIR
 cp $CPP_SOURCE $DESTINATION_DIR
 cp $HPP_SOURCE $DESTINATION_DIR
 
-~/scripts/files/replacer $DESTINATION_DIR/$CLASSNAME.cpp $CLASSNAME
-~/scripts/files/replacer $DESTINATION_DIR/include/$CLASSNAME.hpp $CLASSNAME
+~/useful_scripts/files/replacer $DESTINATION_DIR/$CLASSNAME.cpp $CLASSNAME
+~/useful_scripts/files/replacer $DESTINATION_DIR/include/$CLASSNAME.hpp $CLASSNAME
 rm $DESTINATION_DIR/AClass.cpp
 rm $DESTINATION_DIR/AClass.hpp
 rm $DESTINATION_DIR/header
